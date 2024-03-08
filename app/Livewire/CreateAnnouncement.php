bed7ba08866b547dea08cf8c4741fb360ffb205c
Nicola <nicolaferrigolo@gmail.com>
2024-03-08 16:33:12 +0100

    test

--- app/Livewire/CreateAnnouncement.php
+++ app/Livewire/CreateAnnouncement.php
@@ -7,30 +7,31 @@ use App\Models\Category;
 use App\Models\Announcement;
 use Livewire\Attributes\Validate;
 
+
 class CreateAnnouncement extends Component
 {
     #[Validate('required', message: 'Il campo è obbligatorio')]
     #[Validate('min:5', message: 'Il nome è troppo corto')]
     public $name = '';
-    
+
     #[Validate('required', message: 'Il campo è obbligatorio')]
     #[Validate('min:5', message: 'La descrizione è troppo corta')]
     public $description = '';
-    
-    #[Validate('required', message: 'Il campo è obbligatorio')] 
+
+    #[Validate('required', message: 'Il campo è obbligatorio')]
     public $price = '';
 
     #[Validate('required', message: 'Il campo è obbligatorio')]
-    public $category = ''; 
-    
+    public $category = '';
+
     public function updated($propertyName)
     {
         $this->validateOnly($propertyName);
     }
-    
+
     public function store(){
         $category=Category::find($this->category);
-        $this->validate(); 
+        $this->validate();
         $category->announcements()->create([
             'name'=>$this->name,
             'description'=>$this->description,
@@ -39,9 +40,9 @@ class CreateAnnouncement extends Component
         $this->reset();
         session()->flash('status', 'Annuncio caricato correttamente');
     }
-    
+
     public function render()
-    {   
+    {
         return view('livewire.create-announcement');
     }
 }
