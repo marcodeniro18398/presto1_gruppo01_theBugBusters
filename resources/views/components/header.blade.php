f7154103fd15f6198018709e709600ab8d74c1a3
Marco Sanna <msanna682@gmail.com>
2024-03-19 15:15:56 +0100

    modificato titolo

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -10,12 +10,12 @@
     </div>
     <div class="row py-5">
         <div class="col-12 py-5">
-            <h1 class="color-grey pt-5 text-uppercase font-primary display-5 fw-bold">Presto.it</h1>
+            <h1 class="color-grey  font-title title ">Presto.it</h1>
         </div>
     </div>
     {{-- <div class="justify-content-center"> --}}
     <form action="{{ route('announcements.search') }}" class="row justify-content-center flex-column align-items-center" role="search" method="GET">
-        <div class="col-8 col-md-3 justify-content-center py-2">
+        <div class="col-8 col-md-3 justify-content-center py-3">
             <input class="form-control shadow-input" type="search" placeholder="{{__('ui.search')}}" aria-label="Search" name="searched">
         </div>
         <div class="col-12 col-md-1 d-flex justify-content-center">
