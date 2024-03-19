e2e2064f85eb23f16fab71e110bf7440f57b4ad1
Claudio Gugliotta <claudio.gugliotta6677@gmail.com>
2024-03-20 00:48:57 +0100

    completata user story 8

--- app/Jobs/RemoveFaces.php
+++ app/Jobs/RemoveFaces.php
@@ -35,8 +35,8 @@ class RemoveFaces implements ShouldQueue
         if(!$i){
             return;
         }
+
         $srcPath = storage_path('app/public/' . $i->path); 
-        
         $image = file_get_contents($srcPath);
         
         putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google_credential.json'));
@@ -47,16 +47,26 @@ class RemoveFaces implements ShouldQueue
         
         foreach($faces as $face){
             $vertices=$face->getBoundingPoly()->getVertices();
+
             $bounds = [];
+
             foreach($vertices as $vertex){
-                $bounds[] = [$vertex->getX(),$vertex->getY()];
+                $bounds[] = [$vertex->getX(), $vertex->getY()];
             }
-            $w=$bounds[2][0]-$bounds[0][0];
-            $h=$bounds[2][1]-$bounds[0][1];
-            $image=SpatieImage::load($srcPath);
-            $image->watermark(base_path('resources/img/smile.png'))->watermarkPosition('top-left')->watermarkPadding($bounds[0][0], $bounds[0][1])->watermarkWidth($w, Manipulations::UNIT_PIXELS)->watermarkHeight($h, Manipulations::UNIT_PIXELS)->watermarkFit(Manipulations::FIT_STRETCH);
+
+            $w = $bounds[2][0] - $bounds[0][0];
+            $h = $bounds[2][1] - $bounds[0][1];
+
+            $image = SpatieImage::load($srcPath);
+
+            $image->watermark(base_path('resources/img/smile.png'))
+            ->watermarkPosition('top-left')
+            ->watermarkPadding($bounds[0][0], $bounds[0][1])
+            ->watermarkWidth($w, Manipulations::UNIT_PIXELS)
+            ->watermarkHeight($h, Manipulations::UNIT_PIXELS)
+            ->watermarkFit(Manipulations::FIT_STRETCH);
             
-            $i->save($srcPath);
+            $image->save($srcPath);
         }
         $imageAnnotator->close();
     }
