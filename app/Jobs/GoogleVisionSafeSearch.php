07dd6148d70086d7d4ba3dc9c30d833959d1bfc5
Nicola <nicolaferrigolo@gmail.com>
2024-05-10 12:50:19 +0200

    Commentate le logiche di Google Vision in quanto licenza mancante

--- app/Jobs/GoogleVisionSafeSearch.php
+++ app/Jobs/GoogleVisionSafeSearch.php
@@ -1,68 +1,70 @@
 <?php
 
-namespace App\Jobs;
+//!  NB: Commentata la logica del GoogleSafeSearch in quanto licenza mancante
 
-use App\Models\Image;
-use Illuminate\Bus\Queueable;
-use Illuminate\Queue\SerializesModels;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Contracts\Queue\ShouldBeUnique;
-use Google\Cloud\Vision\V1\ImageAnnotatorClient;
+// namespace App\Jobs;
 
-class GoogleVisionSafeSearch implements ShouldQueue
-{
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+// use App\Models\Image;
+// use Illuminate\Bus\Queueable;
+// use Illuminate\Queue\SerializesModels;
+// use Illuminate\Queue\InteractsWithQueue;
+// use Illuminate\Contracts\Queue\ShouldQueue;
+// use Illuminate\Foundation\Bus\Dispatchable;
+// use Illuminate\Contracts\Queue\ShouldBeUnique;
+// use Google\Cloud\Vision\V1\ImageAnnotatorClient;
 
-    private $article_image_id;
-    /**
-     * Create a new job instance.
-     */
-    public function __construct($article_image_id)
-    {
-        $this->article_image_id = $article_image_id;
-    }
+// class GoogleVisionSafeSearch implements ShouldQueue
+// {
+//     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    /**
-     * Execute the job.
-     */
-    public function handle(): void
-    {
-        $i = Image::find($this->article_image_id);
+//     private $article_image_id;
+//     /**
+//      * Create a new job instance.
+//      */
+//     public function __construct($article_image_id)
+//     {
+//         $this->article_image_id = $article_image_id;
+//     }
 
-        if(!$i){
-            return;
-        }
+//     /**
+//      * Execute the job.
+//      */
+//     public function handle(): void
+//     {
+//         $i = Image::find($this->article_image_id);
 
-        $image = file_get_contents(storage_path('app/public/' . $i->path));
+//         if(!$i){
+//             return;
+//         }
 
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google_credential.json'));
+//         $image = file_get_contents(storage_path('app/public/' . $i->path));
 
-        $imageAnnotator = new ImageAnnotatorClient();
-        $response = $imageAnnotator->safeSearchDetection($image);
-        $imageAnnotator->close();
+//         putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google_credential.json'));
 
-        $safe = $response->getSafeSearchAnnotation();
+//         $imageAnnotator = new ImageAnnotatorClient();
+//         $response = $imageAnnotator->safeSearchDetection($image);
+//         $imageAnnotator->close();
 
-        $adult = $safe->getAdult();
-        $medical = $safe->getMedical();
-        $spoof = $safe->getSpoof();
-        $violence = $safe->getViolence();
-        $racy = $safe->getRacy();
+//         $safe = $response->getSafeSearchAnnotation();
 
-        // echo json_encode([$adult, $medical, $spoof, $violence, $racy]);
+//         $adult = $safe->getAdult();
+//         $medical = $safe->getMedical();
+//         $spoof = $safe->getSpoof();
+//         $violence = $safe->getViolence();
+//         $racy = $safe->getRacy();
 
-        $likelihoodName = [
-            'text-secondary bi bi-circle-fill', 'text-success bi bi-circle-fill', 'text-success bi bi-circle-fill', 'text-warning bi bi-circle-fill', 'text-warning bi bi-circle-fill', 'text-danger bi bi-circle-fill'
-        ];
+//         // echo json_encode([$adult, $medical, $spoof, $violence, $racy]);
 
-        $i->adult = $likelihoodName[$adult];
-        $i->medical = $likelihoodName[$medical];
-        $i->spoof = $likelihoodName[$spoof];
-        $i->violence = $likelihoodName[$violence];
-        $i->racy = $likelihoodName[$racy];
+//         $likelihoodName = [
+//             'text-secondary bi bi-circle-fill', 'text-success bi bi-circle-fill', 'text-success bi bi-circle-fill', 'text-warning bi bi-circle-fill', 'text-warning bi bi-circle-fill', 'text-danger bi bi-circle-fill'
+//         ];
 
-        $i->save();
-    }
-}
+//         $i->adult = $likelihoodName[$adult];
+//         $i->medical = $likelihoodName[$medical];
+//         $i->spoof = $likelihoodName[$spoof];
+//         $i->violence = $likelihoodName[$violence];
+//         $i->racy = $likelihoodName[$racy];
+
+//         $i->save();
+//     }
+// }
--- app/Jobs/RemoveFaces.php
+++ app/Jobs/RemoveFaces.php
@@ -16,7 +16,7 @@ class RemoveFaces implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
     private $announcement_image_id;
-    
+
     /**
     * Create a new job instance.
     */
@@ -24,60 +24,64 @@ class RemoveFaces implements ShouldQueue
     {
         $this->announcement_image_id=$announcement_image_id;
     }
-    
+
     /**
     * Execute the job.
     */
     public function handle(): void
     {
         $i = Image::find($this->announcement_image_id);
-        
+
         if(!$i){
             return;
         }
-        
-        $srcPath = storage_path('app/public/' . $i->path); 
+
+        $srcPath = storage_path('app/public/' . $i->path);
         $image = file_get_contents($srcPath);
-        $logo = file_get_contents($srcPath);
-        
-        putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google_credential.json'));
-        
-        $imageAnnotator = new ImageAnnotatorClient();
-        $response = $imageAnnotator->faceDetection($image);
-        $faces = $response->getFaceAnnotations(); 
-        
-        
-        foreach($faces as $face){
-            $vertices=$face->getBoundingPoly()->getVertices();
-            
-            $bounds = [];
-            
-            foreach($vertices as $vertex){
-                $bounds[] = [$vertex->getX(), $vertex->getY()];
-            }
-            
-            $w = $bounds[2][0] - $bounds[0][0];
-            $h = $bounds[2][1] - $bounds[0][1];
-            
-            $image = SpatieImage::load($srcPath);
-            
-            $image->watermark(base_path('resources/img/smile.png'))
-            ->watermarkPosition('top-left')
-            ->watermarkPadding($bounds[0][0], $bounds[0][1])
-            ->watermarkWidth($w, Manipulations::UNIT_PIXELS)
-            ->watermarkHeight($h, Manipulations::UNIT_PIXELS)
-            ->watermarkFit(Manipulations::FIT_STRETCH);
-            
+
+        //! NB: Commentata la logica del Remove Faces
+        // $logo = file_get_contents($srcPath);
+
+        //!  NB: Commentata la logica del GoogleSafeSearch in quanto licenza mancante
+        // putenv('GOOGLE_APPLICATION_CREDENTIALS=' . base_path('google_credential.json'));
+
+
+        // $imageAnnotator = new ImageAnnotatorClient();
+        // $response = $imageAnnotator->faceDetection($image);
+        // $faces = $response->getFaceAnnotations();
+
+
+        // foreach($faces as $face){
+        //     $vertices=$face->getBoundingPoly()->getVertices();
+
+        //     $bounds = [];
+
+        //     foreach($vertices as $vertex){
+        //         $bounds[] = [$vertex->getX(), $vertex->getY()];
+        //     }
+
+        //     $w = $bounds[2][0] - $bounds[0][0];
+        //     $h = $bounds[2][1] - $bounds[0][1];
+
+        //     $image = SpatieImage::load($srcPath);
+
+        //     $image->watermark(base_path('resources/img/smile.png'))
+        //     ->watermarkPosition('top-left')
+        //     ->watermarkPadding($bounds[0][0], $bounds[0][1])
+        //     ->watermarkWidth($w, Manipulations::UNIT_PIXELS)
+        //     ->watermarkHeight($h, Manipulations::UNIT_PIXELS)
+        //     ->watermarkFit(Manipulations::FIT_STRETCH);
+
             // $image->watermark(base_path('resources/img/logo.png'))
             // ->watermarkPosition('bottom-left')
             // ->watermarkHeight(20, Manipulations::UNIT_PIXELS)
             // ->watermarkWidth(40, Manipulations::UNIT_PIXELS);
-            
-            $image->save($srcPath);
+
+            // $image->save($srcPath);
             // $logo->save($srcPath);
-        }
-        
-        
-        $imageAnnotator->close();
+        // }
+
+
+        // $imageAnnotator->close();
     }
 }
